5a01a3ebfb2e2034b6a07f9b942f22473c61f91f
Andrei Yangabishev <waffleboot@gmail.com>
2022-06-02 22:09:37 +0300

    ginkgo generate book

---
+++ books/book_test.go
@@ -0,0 +1,39 @@
+package books_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/waffleboot/ginkgo-learn/books"
+)
+
+var _ = Describe("Book", func() {
+	var foxInSocks, lesMis *books.Book
+
+	BeforeEach(func() {
+		lesMis = &books.Book{
+			Title:  "Les Miserables",
+			Author: "Victor Hugo",
+			Pages:  2783,
+		}
+
+		foxInSocks = &books.Book{
+			Title:  "Fox In Socks",
+			Author: "Dr. Seuss",
+			Pages:  24,
+		}
+	})
+
+	Describe("Categorizing books", func() {
+		Context("with more than 300 pages", func() {
+			It("should be a novel", func() {
+				Expect(lesMis.Category()).To(Equal(books.CategoryNovel))
+			})
+		})
+		Context("with fewer than 300 pages", func() {
+			It("should be a short story", func() {
+				Expect(foxInSocks.Category()).To(Equal(books.CategoryShortStory))
+			})
+		})
+	})
+})
