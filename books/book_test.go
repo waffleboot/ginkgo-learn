f104cc11dd3773df5cf6842dd00f04f313c5a768
Andrei Yangabishev <waffleboot@gmail.com>
2022-06-04 16:33:39 +0300

    describe table

--- books/book_test.go
+++ books/book_test.go
@@ -19,54 +19,21 @@ var _ = Describe("Books", func() {
 		Expect(book.IsValid()).To(BeTrue())
 	})
 
-	Describe("Extracting the author's first and last name", func() {
-		Context("When the author has both names", func() {
-			It("can extract the author's last name", func() {
-				Expect(book.AuthorLastName()).To(Equal("Hugo"))
-			})
-
-			It("can extract the author's first name", func() {
-				Expect(book.AuthorFirstName()).To(Equal("Victor"))
-			})
-		})
-
-		Context("When the author only has one name", func() {
-			BeforeEach(func() {
-				book.Author = "Hugo"
-			})
-
-			It("interprets the single author name as a last name", func() {
-				Expect(book.AuthorLastName()).To(Equal("Hugo"))
-			})
-
-			It("returns empty for the first name", func() {
-				Expect(book.AuthorFirstName()).To(BeZero())
-			})
-		})
-
-		Context("When the author has a middle name", func() {
-			BeforeEach(func() {
-				book.Author = "Victor Marie Hugo"
-			})
-
-			It("can extract the author's last name", func() {
-				Expect(book.AuthorLastName()).To(Equal("Hugo"))
-			})
-
-			It("can extract the author's first name", func() {
-				Expect(book.AuthorFirstName()).To(Equal("Victor"))
-			})
-		})
-
-		Context("When the author has no name", func() {
-			It("should not be a valid book and returns empty for first and last name", func() {
-				book.Author = ""
-				Expect(book.IsValid()).To(BeFalse())
-				Expect(book.AuthorLastName()).To(BeZero())
-				Expect(book.AuthorFirstName()).To(BeZero())
-			})
-		})
-	})
+	DescribeTable("Extracting the author's first and last name",
+		func(author string, isValid bool, firstName string, lastName string) {
+			book := &books.Book{
+				Title:  "My Book",
+				Author: author,
+				Pages:  10,
+			}
+			Expect(book.IsValid()).To(Equal(isValid))
+			Expect(book.AuthorFirstName()).To(Equal(firstName))
+			Expect(book.AuthorLastName()).To(Equal(lastName))
+		},
+		Entry("When the author has both names", "Victor Hugo", true, "Victor", "Hugo"),
+		Entry("When the author only has one name", "Hugo", true, "", "Hugo"),
+		Entry("When the author has a middle name", "Victor Marie Hugo", true, "Victor", "Hugo"),
+		Entry("When the author has no name", "", false, "", ""))
 
 	Describe("JSON encoding and decoding", func() {
 		It("survives the round trip", func() {
